6fe92c52b74d79433289a7ee52dd9b092584e202
Aquila Payton <44341215+Apayton1@users.noreply.github.com>
2019-07-06 21:59:44 -0400

    I added a background image, forms, id's and classes as needed, and added the code that pulls the info from the form to the database and dynamically updates the browser

--- assets/app.js
+++ assets/app.js
@@ -1,58 +1,68 @@
-  // Your web app's Firebase configuration
-  var firebaseConfig = {
-    apiKey: "AIzaSyATbmCERD6MJd8jZOcJy_qTTXYGkPQRPrQ",
-    authDomain: "apayton-baaf4.firebaseapp.com",
-    databaseURL: "https://apayton-baaf4.firebaseio.com",
-    projectId: "apayton-baaf4",
-    storageBucket: "",
-    messagingSenderId: "204631347713",
-    appId: "1:204631347713:web:6adeaec970c21b1f"
-  };
-  // Initialize Firebase
-  firebase.initializeApp(firebaseConfig);
+// Your web app's Firebase configuration
+var firebaseConfig = {
+  apiKey: "AIzaSyATbmCERD6MJd8jZOcJy_qTTXYGkPQRPrQ",
+  authDomain: "apayton-baaf4.firebaseapp.com",
+  databaseURL: "https://apayton-baaf4.firebaseio.com",
+  projectId: "apayton-baaf4",
+  storageBucket: "",
+  messagingSenderId: "204631347713",
+  appId: "1:204631347713:web:6adeaec970c21b1f"
+};
+// Initialize Firebase
+firebase.initializeApp(firebaseConfig);
 
-$("#submit").on("click",function(){
 
-    var trainName = $("#trainNameText").val();
-    var destination = $("#destinationText").val();
-    var firstTrain = $("#firstTrainText").val();
-    var frequency = $("#frequencyText").val();
+
+
+$("#submit").on("click", function () {
+
+  var trainName = $("#trainNameText").val();
+  var destination = $("#destinationText").val();
+  var firstTrain = $("#firstTrainText").val();
+  var frequency = $("#frequencyText").val();
+  var currentTime = moment();
+  console.log(moment());
+  var firstTrainConverted = moment(firstTrain, "hh:mm").subtract("1, years");
+  var difference = currentTime.diff(moment(firstTrain));
+  var minutesAway =
+
+
 
     console.log('Name: ' + trainName);
-    console.log('Destination: ' + destination);
-    console.log('First Train: ' + firstTrain);
-    console.log('Frequency: ' + frequency);
+  console.log('Destination: ' + destination);
+  console.log('First Train: ' + firstTrain);
+  console.log('Frequency: ' + frequency);
 
-    firebase.database().ref().push({
-        name: trainName,
-        destination: destination,
-        frequency: frequency,
-        firstTrainTime: firstTrain
-    });
+  firebase.database().ref().push({
+    name: trainName,
+    destination: destination,
+    frequency: frequency,
+    firstTrainTime: firstTrain
+  });
 
-    /// Clear input elements.
-    $("#trainNameText").val("");
-    $("#destinationText").val("");
-    $("#firstText").val("");
-    $("#frequencyText").val("");
+  /// Clear text elements.
+  $("#trainNameText").val("");
+  $("#destinationText").val("");
+  $("#firstText").val("");
+  $("#frequencyText").val("");
 })
 
-var minutesAway = 
 
-firebase.database().ref().on('child_added', function(snapshot){
-    /// Get the current item.
-    var current = snapshot.val();
 
-    /// Display the values from the current item as a row in the train schedules' table.
-    var trainName = current.name;
-    var destination = current.destination;
-    var firstTrain = current.firstTrainTime;
-    var frequency = current.frequency;
-    //var minutesAway = 
-    
+firebase.database().ref().on('child_added', function (snapshot) {
+  /// Get the current item.
+  var current = snapshot.val();
 
-    $("#trainSchedulesTable").append("<tr><td>" + trainName + '</td><td>' + destination + '</td><td>' + firstTrain + '</td><td>' + frequency + "</td><td></td>")
+  /// Display the values from the current item as a row in the train schedules' table.
+  var trainName = current.name;
+  var destination = current.destination;
+  var firstTrain = current.firstTrainTime;
+  var frequency = current.frequency;
+  //var minutesAway = 
 
-    /// Display current item in the console.
-    console.log( current );
+
+  $("#trainSchedulesTable").append("<tr><td>" + trainName + '</td><td>' + destination + '</td><td>' + firstTrain + '</td><td>' + frequency + "</td><td></td>")
+
+  /// Display current item in the console.
+  console.log(current);
 });
